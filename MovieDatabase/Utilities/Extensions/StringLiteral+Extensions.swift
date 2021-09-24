f22170b100ec3bce2d1846daeee4d9b565a34e67
Hiroshi Minamoto <srhiromin@gmail.com>
2021-09-24 15:29:30 -0400

    Add first screen

---
+++ MovieDatabase/Utilities/Extensions/StringLiteral+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  StringLiteral+Extensions.swift
+//  MovieDatabase
+//
+//  Created by Hiroshi Minamoto on 24/09/21.
+//
+
+import Foundation
+
+extension StringLiteralType {
+    static let empty = ""
+}
---
+++ MovieDatabase/ViewModel/MovieDetailViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MovieDetailViewModel.swift
+//  MovieDatabase
+//
+//  Created by Hiroshi Minamoto on 24/09/21.
+//
+
+import Foundation
+import Combine
+
+class MovieDetailViewModel: ObservableObject {
+    
+}
